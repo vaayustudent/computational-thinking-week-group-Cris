10ac7603f8503df371231267e5f1b54dd20d4f5d
MCstarVK <vaayu.kapoor@student.uva.nl>
2026-08-31 12:19:30 +0200

    Fixed spelling mistakes and grammar mistakes in the three acts.

--- challenge_day3/sb.py
+++ challenge_day3/sb.py
@@ -2,10 +2,10 @@ def get_name():
     return "Jingqi Xue"
 
 def Jingqi_act_one():
-    return "Fili, Kili, Dori, Nori and Ori wnat to steal some golds from Thorin to buy some beers and find a wife. They know Thorin is a big heo and they can not beat Thorin, but they still stat the journey  of stealing gold. And maybe they can also find “the balls”. They managed to escape the goblins cave, rushing back to the party bus. They connect to the speakers and play 2010’s hits on spotify."
+    return f"Fili, Kili, Dori, Nori and Ori want to steal some gold from Thorin to buy some beers and find a wife. They know Thorin is a big hero and they cannot beat Thorin, but they still start the journey of stealing gold. And maybe they can also find "the balls". They managed to escape the goblins' cave, rushing back to the party bus. They connect to the speakers and play 2010's hits on Spotify."
 
 def Jingqi_act_two():
-    return "They arrive at lonely mountain, but sadly because of the tumbbleee arund in the bus, Nori can not fight anymore, Fili, Kili, Dori and Ori decide to not given Nori gold after the steal it , luckly Nori could still able to drink beers. "
+    return "They arrive at Lonely Mountain, but sadly because of the tumble around in the bus, Nori cannot fight anymore. Fili, Kili, Dori and Ori decide not to give Nori gold after they steal it, luckily Nori could still drink beers."
 
 def Jingqi_act_three():
-    return "Roll and roll and roll and roll,nothing could stop the feeling of achoclol. “Maybe i can find a wife in the party” Fili said. He is too drunk that he can not see anything clearly, he yalling :”Whoever you are, as long as you're a woman and willing to marry me, we'll get married right here today!” In the end, Fili and the goblin lady, the only one who agreed to marry him that night, lived a happy life together."
+    return "Roll and roll and roll and roll, nothing could stop the feeling of alcohol. "Maybe I can find a wife in the party," Fili said. He is too drunk that he cannot see anything clearly, he yelling: "Whoever you are, as long as you're a woman and willing to marry me, we'll get married right here today!" In the end, Fili and the goblin lady, the only one who agreed to marry him that night, lived a happy life together."
