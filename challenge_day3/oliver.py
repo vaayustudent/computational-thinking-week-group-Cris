8ba3d6c89e2ad63f3dff419d4f5cc468061acf69
Oliver Dodgson <oliver.s.dodgson@gmail.com>
2026-08-31 12:01:03 +0200

    I added my story paragraphs, with typos

--- challenge_day3/oliver.py
+++ challenge_day3/oliver.py
@@ -1,2 +1,14 @@
 def get_name():
     return "Oliver"
+
+# character names: fili kili dori nori ori
+
+def act_one_oliver():
+    return "Fili, Kili, Dori, Nori and Ori gzed out the wndow of ther party bus as they bgan ther  long drve towards the lonely mountain. Fili remarked how he mised Gandalf’s suply of pipe-weed, with Kili qckly agreeing. As thy sat quietly on th bus, Nori and Ori spontaneously bgan to break out into sng and danc, snging the story of Thorin Oakshield."
+
+def act_two_oliver():
+    return "As Fili and Kili hagng on to their    seats, Nori and Ori tumbbleee arund the bus as the muntain around them begins to crack and shak. Dori shouts “look out” as the drivr of the beus accelerates, narrowly avoidng the boulder heading straight fooor their bus. Fili and Kili embrace ech othr and celebrte their close encountr with the great mountin giantsssss."
+
+def act_three_oliver():
+    return "Dori falls off the table in his drunkn state, and coincidentlly knocks ovr a vase containing copiuss amounts of pipe-weed. Fili and Kili exclm in happinesssss, as Ori and Nori prparee themselves mentally for the night that is to come. As the Dwarves begn to smoke, thy     huddle in a corcle and begn to recounnttt their past advrnuts."
+
