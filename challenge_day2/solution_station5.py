5c0eb7bfdd7c571a86ef986cff3c6129bc324840
crisupsmart <cris.li@student.uva.nl>
2026-09-01 12:39:27 +0200

    name name

--- challenge_day2/solution_station5.py
+++ challenge_day2/solution_station5.py
@@ -5,3 +5,27 @@
 #       self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Lora', 9, 'a+b'), 1.00544313459462e+16)
 #        self.io_pair(('00:02', 6, '1990-01-02', 4, 2 (LT), 'Iris (name)', 8, 'b+c'), 4.827884479828662e+16)
 #        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Minseo', 7, 'd*e'), 6.07785634489743e+16)
+
+LT1 = [
+    "Ainas", "Tobit", "Yasmin", "Zoë", "Luliia", "Klementyna", "Markus", "Mufang",
+    "Oumaima", "Ebony", "Nandini", "Nathan", "Tiara", "Yurui", "Ben", "Christopher",
+    "Lula", "Muni", "Yuvraj"
+]
+
+LT2 = [
+    "Alex", "Arwen", "Rajko", "Sylwia", "Zeno", "Iris", "Katharina", "Minseo",
+    "Sade", "Huy", "Christina", "Helen", "Mark", "Mats", "Vadim", "David",
+    "Lora", "Quinn", "Tarling"
+]
+
+LT3 = [
+    "Elizabeth", "Gabriel", "Jakub", "Luc", "Soelie", "Aleksandra", "Arnav",
+    "Donna", "Milan", "Rongze", "Cris", "Jingqi", "Oliver", "Vaayu", "Yusef",
+    "Afua", "Anna", "Daniel", "Nataly", "Rafael"
+]
+
+LT4 = [
+    "Jeremy", "Krishiv", "Neel", "Yujie", "Yutong", "An Nguyen", "Heer",
+    "Paige", "Samir", "Amalia", "Douwe", "Ilya", "Maria", "Rakin", "Lara",
+    "Lucas", "Michelle", "Oliwia", "Tom"
+]
