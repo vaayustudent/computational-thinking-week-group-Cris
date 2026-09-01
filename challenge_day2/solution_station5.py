1266911e1a17ceafad54f22dc9be92e8793ea9f9
crisupsmart <cris.li@student.uva.nl>
2026-09-01 12:26:47 +0200

    Update solution_station5.py

--- challenge_day2/solution_station5.py
+++ challenge_day2/solution_station5.py
@@ -1,2 +1,7 @@
 #5 start from here, name output
 
+##class Test_Exercise_Values(edutest.TestCase):
+#    def test(self):
+#       self.io_pair(('00:01', 5, '1990-01-01', 3, 1, 'Lora', 9, 'a+b'), 1.00544313459462e+16)
+#        self.io_pair(('00:02', 6, '1990-01-02', 4, 2 (LT), 'Iris', 8, 'b+c'), 4.827884479828662e+16)
+#        self.io_pair(('00:03', 7, '1990-01-03', 5, 3, 'Minseo', 7, 'd*e'), 6.07785634489743e+16)
