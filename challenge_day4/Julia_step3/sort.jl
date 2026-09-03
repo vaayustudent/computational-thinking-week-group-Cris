606a6b84945a6bd17bba75632da8429668e66719
vaayu <vaayu.kapoor@student.uva.nl>
2026-09-03 09:53:00 +0200

    added the julia files

--- challenge_day4/Julia_step3/sort.jl
+++ challenge_day4/Julia_step3/sort.jl
@@ -1,7 +1,7 @@
 using DataFrames, CSV, Statistics, DelimitedFiles
 
 # Read the CSV file into a DataFrame
-people_df = CSV.File("data3.csv") |> DataFrame
+people_df = CSV.File("C:\\Users\\vaayu\\computational-thinking-week-group-Cris\\challenge_day4\\testdata\\data3.csv") |> DataFrame
 
 # Function to classify a score based on quartiles
 function classify_score(score, quartiles)
@@ -32,12 +32,12 @@ for col_name in names(people_df)[2:end]
     quartiles = quantile(valid_data, [0.25, 0.5, 0.75])
 
     # Replace values with categories or 'low' if they are Float64
-    new_col = map(x -> x isa Float64 ? "low" : classification_score(x, quartiles), col_data)
+    new_col = map(x -> x isa Float64 ? "low" : classify_score(x, quartiles), col_data)
     people_df[!, col_name] = new_col
 end
 
 # Save the modified DataFrame back to a new CSV file
-CSV.write("data4.txt", people_df)
+CSV.write("C:\\Users\\vaayu\\computational-thinking-week-group-Cris\\challenge_day4\\Julia_step3\\data4.csv", people_df)
 
 # Save the modified DataFrame back to a new TXT file
 #writedlm("data4.txt", people_df, ',')
