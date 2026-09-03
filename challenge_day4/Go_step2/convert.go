4d04e8cb9bae24351e584799ea0e62d43eb35b64
oliversdodgson-ops <oliver.s.dodgson@gmail.com>
2026-09-03 10:30:48 +0200

    fixed the technical method/skill name

--- challenge_day4/Go_step2/convert.go
+++ challenge_day4/Go_step2/convert.go
@@ -9,12 +9,12 @@ import (
 )
 
 type Person struct {
-	Name           string `json:"name"`
-	TechnicalSkills float64    `json:"Technical methods"`
-	SoftSkills      float64    `json:"Soft Skills"`
-	BusinessSkills  float64    `json:"Business Skills"`
-	CreativeSkills  float64    `json:"Creative Skills"`
-	AcademicSkills  float64    `json:"Academic Skills"`
+	Name            string  `json:"name"`
+	TechnicalSkills float64 `json:"Technical Skills"` //this was earlier technical methods, not technical skills
+	SoftSkills      float64 `json:"Soft Skills"`
+	BusinessSkills  float64 `json:"Business Skills"`
+	CreativeSkills  float64 `json:"Creative Skills"`
+	AcademicSkills  float64 `json:"Academic Skills"`
 }
 
 type People struct {
